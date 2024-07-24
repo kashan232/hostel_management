8251df31ebad675502d9bf18127a799f6c8f98e0
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-07-25 00:29:07 +0500

    changes

--- resources/views/admin_panel/inlcude/sidebar_include.blade.php
+++ resources/views/admin_panel/inlcude/sidebar_include.blade.php
@@ -1,18 +1,12 @@
 <div class="dlabnav">
     <div class="dlabnav-scroll">
         <ul class="metismenu" id="menu">
-            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+            <li><a href="javascript:void(0);" aria-expanded="false">
                     <i class="material-symbols-outlined">home</i>
                     <span class="nav-text">Dashboard</span>
                 </a>
-                <ul aria-expanded="false">
-                    <li><a href="index.html">Dashboard Light</a></li>
-                    <li><a href="index-2.html">Dashboard Dark</a></li>
-                    <li><a href="finance.html">Finance</a></li>
-                </ul>
-
             </li>
-            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+            <li><a class="has-arrow" href="javascript:void(0);" aria-expanded="false">
                     <i class="material-symbols-outlined">school</i>
                     <span class="nav-text">Student</span>
                 </a>
