48ea8aa5fce7acc9dd2c4b91662d82eb408b6375
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-07-25 00:36:50 +0500

    changes

--- resources/views/admin_panel/inlcude/navbar_include.blade.php
+++ resources/views/admin_panel/inlcude/navbar_include.blade.php
@@ -169,14 +169,21 @@
 
                                     </div>
                                     <div class="card-footer text-center p-3">
-                                        <a href="page-login.html" class="dropdown-item ai-icon btn btn-primary light">
+
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+                                        <a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="dropdown-item ai-icon btn btn-primary light">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="var(--primary)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                                 <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
                                                 <polyline points="16 17 21 12 16 7"></polyline>
                                                 <line x1="21" y1="12" x2="9" y2="12"></line>
                                             </svg>
-                                            <span class="ms-2 text-primary">Logout </span>
+                                            <span class="ms-2 text-primary">{{ __('Log Out') }}  </span>
                                         </a>
+
+                                    </form>
+
+                                        
 
                                     </div>
                                 </div>
