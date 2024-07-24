ef859c43330b9fe4428996c1962f39ec0f30b5b3
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-07-24 17:26:37 +0500

    routes changes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// Github Connected
 Route::get('/', function () {
     return view('welcome');
 });
